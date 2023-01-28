b4a9349e430707351b0d04b9e2b4fbd7d55968e4
Andrei <a.g.popov@hotmail.com>
2023-01-28 17:52:48 +0100

    query in process

--- homework/exercise_1/app.js
+++ homework/exercise_1/app.js
@@ -4,19 +4,30 @@ import mysql from 'mysql';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-let connection = mysql.createConnection({
+const conDb = mysql.createConnection({
   host: 'localhost',
   user: 'hyfuser',
   password: 'hyfpassword',
 });
 
-connection.connect((err) => {
+conDb.connect((err) => {
   if (err) {
     console.error('error connecting to database: ' + err.stack);
     return;
   }
 
   console.log('connected to database as id ' + connection.threadId);
+});
+
+const sql = 'sql';
+const values = [
+  ['', ''],
+  ['', ''],
+];
+
+conDb.query(sql, [values], (error, result) => {
+  if (error) throw error;
+  console.log('query done:' + result.affectedRows);
 });
 
 app.listen(PORT, console.log(`Server started on port: ${PORT}`));
