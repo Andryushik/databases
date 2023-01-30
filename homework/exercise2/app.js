8f1758bd0dafdd3955ba9316f9df96b26964e20f
Andrei <a.g.popov@hotmail.com>
2023-01-30 19:49:04 +0100

    1sr query

--- homework/exercise2/app.js
+++ homework/exercise2/app.js
@@ -4,20 +4,40 @@ import mysql from 'mysql';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const poolWorldDb = mysql.createPool({
-  connectionLimit: 10,
+const conWorldDb = mysql.createConnection({
   host: 'localhost',
   user: 'hyfuser',
   password: 'hyfpassword',
   database: 'new_world',
 });
 
-poolWorldDb.query(
+conWorldDb.connect((error) => {
+  if (error) {
+    console.error('Error connecting to World DB: ' + error.stack);
+    return;
+  }
+  console.log('Connected to World DB as id ' + conWorldDb.threadId);
+});
+
+conWorldDb.query(
   'SELECT Name FROM country WHERE Population > 8000000;',
   function (error, results, fields) {
     if (error) throw error;
-    console.log(results);
+    console.log(
+      'What are the names of countries with population greater than 8 million?',
+    );
+    results.forEach((element) => {
+      console.log(element.Name);
+    });
   },
 );
+
+conWorldDb.end((error) => {
+  if (error) {
+    console.error('cannot disconnect: ' + error.stack);
+    return;
+  }
+  console.log('Successfully disconnected from World DB.');
+});
 
 app.listen(PORT, console.log(`Server started on port: ${PORT}`));
