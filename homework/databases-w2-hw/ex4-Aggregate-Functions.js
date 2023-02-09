8a3f40ac8dae279d9e8fbb552bb45b7a759c27e5
Andrei <a.g.popov@hotmail.com>
2023-02-09 14:16:57 +0100

    deleted server part

--- homework/databases-w2-hw/ex4-Aggregate-Functions.js
+++ homework/databases-w2-hw/ex4-Aggregate-Functions.js
@@ -1,8 +1,4 @@
-import express from 'express';
 import mysql from 'mysql';
-
-const app = express();
-const PORT = process.env.PORT || 3000;
 
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -83,5 +79,3 @@ WHERE gender = 'Female';`,
 };
 
 executeQueries();
-
-app.listen(PORT, console.log(`Server started on port: ${PORT}`));
