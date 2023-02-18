2f5475b99d0cd5aa2443bd20db0c4b2eff919a7f
Andrei <a.g.popov@hotmail.com>
2023-02-18 19:34:52 +0100

    ex2 done

---
+++ Week4/homework/ex2-transactions/index.js
@@ -0,0 +1,45 @@
+import { MongoClient, ServerApiVersion } from 'mongodb';
+import * as dotenv from 'dotenv';
+import { addCollections } from './setup.js';
+import { transferCredits } from './transfer.js';
+
+dotenv.config();
+
+const mongoUrl = process.env.MONGODB_URL;
+
+async function main() {
+  if (mongoUrl == null) {
+    throw Error(`You did not set up the environment variables correctly.`);
+  }
+  const client = new MongoClient(mongoUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+  });
+
+  try {
+    await client.connect();
+    console.log('Connected to databaseWeek4');
+    await addCollections(client);
+    await transferCredits(client, 101, 102, 1000);
+    await transferCredits(client, 105, 102, 2900);
+  } catch (err) {
+    console.error(err);
+  } finally {
+    client.close();
+    console.log('Connection closed');
+  }
+}
+
+async function populationOfCountry(client, country = 'Netherlands') {
+  const agg = [];
+
+  const populationList = await client
+    .db('databaseWeek4')
+    .collection('population')
+    .aggregate(agg)
+    .toArray();
+  console.log(populationList);
+}
+
+main();
---
+++ Week4/homework/ex2-transactions/transfer.js
@@ -0,0 +1,56 @@
+export async function transferCredits(
+  client,
+  fromAccountN,
+  toAccountN,
+  amount,
+) {
+  const accountsCollection = client.db('databaseWeek4').collection('accounts');
+  const transfersCollection = client
+    .db('databaseWeek4')
+    .collection('transfers');
+  const session = client.startSession();
+
+  try {
+    await session.withTransaction(async () => {
+      await accountsCollection.updateOne(
+        { accountN: fromAccountN },
+        { $inc: { balance: amount * -1 } },
+        { session },
+      );
+
+      await accountsCollection.updateOne(
+        { accountN: toAccountN },
+        { $inc: { balance: amount } },
+        { session },
+      );
+
+      await transfersCollection.insertOne(
+        {
+          accountN: fromAccountN,
+          amount: amount * -1,
+          changedDate: new Date(),
+          remark: 'transfer',
+        },
+        { session },
+      );
+
+      await transfersCollection.insertOne(
+        {
+          accountN: toAccountN,
+          amount: amount,
+          changedDate: new Date(),
+          remark: 'transfer',
+        },
+        { session },
+      );
+    });
+    console.log(
+      `\x1B[33mTransaction from account number ${fromAccountN} to account number ${toAccountN} successful, amount €${amount}.\x1b[m`,
+    );
+  } catch (err) {
+    console.error('\x1B[31mTransaction aborted!!!\x1b[m   ' + err);
+    await session.abortTransaction();
+  } finally {
+    await session.endSession();
+  }
+}
